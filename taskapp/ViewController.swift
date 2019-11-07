8ece9f96eb83507de4461d14125c3e9b562b75c1
yamaguchi_shota <yamaguchi_shota@kakaku.com>
2019-11-07 10:26:24 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 山口 彰太 on 2019/11/07.
+//  Copyright © 2019 shouta.yamaguchi4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
